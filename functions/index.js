50af79610d2dc62c12d0c71e0952d026081700fc
joblolin95 <blaineo_95@yahoo.com>
2017-10-24 19:50:08 -0400

    - poc code for getting and validating difficulty - poc code for loading json into memory

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const functions = require('firebase-functions');
+let test = require('./test.json');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-
+const difficulties = ["easy", "medium", "hard"];
 
 const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
 
@@ -18,7 +19,10 @@ exports.helloWorld = functions.https.onRequest((req, res) => {
     let intent = app.getIntent();
     switch (intent) {
       case app.StandardIntents.MAIN:
-        app.ask('Welcome! Say a number.');
+        const inputPrompt = app.buildInputPrompt(true, '<speak>Welcome to the O and M Trivia Game! Begin the game by choosing ' +
+          'a difficulty. You may choose easy, medium, or hard</speak>',
+                                ['I didn\'t hear a difficulty', 'If you\'re still there, what\'s the difficulty?', 'What is the difficulty?']);
+        app.ask(inputPrompt);
         break;
 
       case app.StandardIntents.TEXT:
@@ -27,13 +31,25 @@ exports.helloWorld = functions.https.onRequest((req, res) => {
         if (app.getRawInput() === 'bye') {
           app.tell('Goodbye!');
         } else {
-        const inputPrompt = app.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">' +
-                                app.getRawInput() + '</say-as></speak>',
+          const response = app.getRawInput();
+          if(difficulties.indexOf(response) < 0){
+            const inputPrompt = app.buildInputPrompt(true, '<speak>You said, ' +
+                                response + '. Please select easy, medium, or hard</speak>',
+                                ['I didn\'t hear a difficulty', 'If you\'re still there, what\'s the difficulty?', 'What is the difficulty?']);
+            app.ask(inputPrompt);
+          } else {
+            const inputPrompt = app.buildInputPrompt(true, '<speak>You said, ' +
+                                app.getRawInput() + '</speak>',
                                 ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
-        app.ask(inputPrompt);
+            app.ask(inputPrompt);
+          }
         }
         break;
     }
+    console.log("Test %j", test);
+    const inputPrompt = app.buildInputPrompt(true, '<speak>Does it work? </speak>',
+    ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+    app.ask(inputPrompt);
   }
   
   // you can add the function name instead of an action map
